d6eab6c61dc4a1596b84d85c61dd9a960807b49d
Jakub Jakubski <36208687+tagisgame@users.noreply.github.com>
2020-06-02 10:34:42 +0200

    feat(UF): add int[] sz attrib

--- dynamic_connectivity/src/us/awpwo/UF.java
+++ dynamic_connectivity/src/us/awpwo/UF.java
@@ -3,9 +3,11 @@ package us.awpwo;
 public class UF {
 
 	protected int[] id;
+  private int[] sz;
 
 	public UF(int N) {
 		id = new int[N];
+    sz = new int[N];
 		for (int i = 0; i < N; ++i) {
 			id[i] = i;
 		}
